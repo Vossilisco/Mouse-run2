92390e8cd17dd9232f09fd4d56fd5cbc1cb689c8
Rafael M <un_segundo@hotmail.com>
2016-04-06 14:08:30 +0200

    Added files via upload

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # Mouse-run
-Practice 1 ,to Artificial intelligence
-
-The first practice we should make an artificial intellifence that will represented by a mouse, to find the biggest
-number of cheeses, but this one is just to develop and strategy to exploring, never getting stuck or making cycles.
-Here you  have an direct link with my  mouse's code::[TonyWheel.java](src/mouserun/mouse/M16B11.java)
+Practice 2 ,to Artificial intelligence
--- src/mouserun/mouse/freeyuyee.java
+++ src/mouserun/mouse/freeyuyee.java
@@ -14,7 +14,7 @@ public class freeyuyee extends Mouse {
     HashMap<Integer, Grid> maparecorrido;
     Stack<Integer> memoria;
     Stack<Integer> colaCamino;
-    Stack<Integer> otraRuta;
+    Stack<Grid> otraRuta;
 
     /*Constructor para dar nombre al raton*/
     public freeyuyee() {
@@ -23,8 +23,7 @@ public class freeyuyee extends Mouse {
         maparecorrido = new HashMap<Integer, Grid>();
         memoria = new Stack<Integer>();
         colaCamino = new Stack<Integer>();
-        otraRuta = new Stack<Integer>();
-
+        otraRuta = new Stack<Grid>();
     }
 
     private static int clavemapa(int x, int y) {
@@ -42,73 +41,80 @@ public class freeyuyee extends Mouse {
             mapa.put(clavemapa(ratonX, ratonY), currentGrid);
 
             if (currentGrid.canGoUp() && mapa.get(clavemapa(ratonX, ratonY + 1)) == null) {
-                otraRuta.push(UP);
+                Grid casilla = new Grid(ratonX, ratonY + 1);
+                otraRuta.push(casilla);
+            }
+            if (currentGrid.canGoRight() && mapa.get(clavemapa(ratonX + 1, ratonY)) == null) {
+                Grid casilla = new Grid(ratonX + 1, ratonY);
+                otraRuta.push(casilla);
+            }
+            if (currentGrid.canGoDown() && mapa.get(clavemapa(ratonX, ratonY - 1)) == null) {
+                Grid casilla = new Grid(ratonX, ratonY - 1);
+                otraRuta.push(casilla);
+            }
+            if (currentGrid.canGoLeft() && mapa.get(clavemapa(ratonX - 1, ratonY)) == null) {
+                Grid casilla = new Grid(ratonX - 1, ratonY);
+                otraRuta.push(casilla);
+
+            }
+        }
+
+            Grid casillaArriba = new Grid(ratonX, ratonY + 1);
+            Grid casillaAbajo = new Grid(ratonX, ratonY - 1);
+            Grid casillaIzq = new Grid(ratonX - 1, ratonY);
+            Grid casillaDer = new Grid(ratonX + 1, ratonY);
+            Grid siguienteCasilla = otraRuta.firstElement();
+            if (siguienteCasilla == casillaArriba) {
                 colaCamino.push(DOWN);
+                otraRuta.pop();
+                return UP;
             }
-            if (currentGrid.canGoRight() && mapa.get(clavemapa(ratonX + 1, ratonY)) == null)  {
-                otraRuta.push(RIGHT);
-                colaCamino.push(LEFT);
-            }
-            if (currentGrid.canGoDown() && mapa.get(clavemapa(ratonX, ratonY - 1)) == null)  {
-                otraRuta.push(DOWN);
+            if (siguienteCasilla == casillaAbajo) {
                 colaCamino.push(UP);
+                otraRuta.pop();
+                return DOWN;
             }
-            if (currentGrid.canGoLeft() && mapa.get(clavemapa(ratonX - 1, ratonY)) == null)  {
-                otraRuta.push(LEFT);
+            if (siguienteCasilla == casillaIzq) {
                 colaCamino.push(RIGHT);
+                otraRuta.pop();
+                return LEFT;
             }
-        }
-        
-        if (otraRuta.empty())
+            if (siguienteCasilla == casillaDer) {
+                colaCamino.push(LEFT);
+                otraRuta.pop();
+                return RIGHT;
+            }
+
             return colaCamino.pop();
-        //DE LA COLA HAY QUE TIRARR SI POSIBLES RUTAS ESTA VACIO
-        else
-        //if (!otraRuta.empty()){
-            /**if (otraRuta.firstElement()==1)
-                colaCamino.push(2);
-            if (otraRuta.firstElement()==2)
-                colaCamino.push(1);
-            if (otraRuta.firstElement()==3)
-                colaCamino.push(4);
-            if (otraRuta.firstElement()==4)
-                colaCamino.push(3); */
-            return otraRuta.pop(); 
-        }
-
+            /**
+             * if (otraRuta.empty()) { return colaCamino.pop(); } //DE LA COLA
+             * HAY QUE TIRARR SI POSIBLES RUTAS ESTA VACIO else //if
+             * (!otraRuta.empty()){ /** if (otraRuta.firstElement()==1)
+             * colaCamino.push(2); if (otraRuta.firstElement()==2)
+             * colaCamino.push(1); if (otraRuta.firstElement()==3)
+             * colaCamino.push(4); if (otraRuta.firstElement()==4)
+             * colaCamino.push(3);
+             */
         
-        
-        
-        
-       /** if (maparecorrido.get(clavemapa(ratonX, ratonY)) == null) {
-            incExploredGrids();
-            maparecorrido.put(clavemapa(ratonX, ratonY), currentGrid);
-        }
+    }
 
-        if (currentGrid.canGoDown() && (mapa.get(clavemapa(ratonX, ratonY - 1)) == null)) {
-            memoria.push(1);
-            return Mouse.DOWN;
-        }
-        if (currentGrid.canGoUp() && mapa.get(clavemapa(ratonX, ratonY + 1)) == null) {
-            memoria.push(2);
-            return Mouse.UP;
-        }
-
-        if (currentGrid.canGoRight() && mapa.get(clavemapa(ratonX + 1, ratonY)) == null) {
-            memoria.push(3);
-            return Mouse.RIGHT;
-        }
-        if (currentGrid.canGoLeft() && mapa.get(clavemapa(ratonX - 1, ratonY)) == null) {
-            memoria.push(4);
-            return Mouse.LEFT;
-        }
-        if (!memoria.empty()) {
-            return memoria.pop();
-        } else {
-            mapa = new HashMap<Integer, Grid>();
-        }
-        return Mouse.BOMB;*/
-     
-
+    /**
+     * if (maparecorrido.get(clavemapa(ratonX, ratonY)) == null) {
+     * incExploredGrids(); maparecorrido.put(clavemapa(ratonX, ratonY),
+     * currentGrid); }
+     *
+     * if (currentGrid.canGoDown() && (mapa.get(clavemapa(ratonX, ratonY - 1))
+     * == null)) { memoria.push(1); return Mouse.DOWN; } if
+     * (currentGrid.canGoUp() && mapa.get(clavemapa(ratonX, ratonY + 1)) ==
+     * null) { memoria.push(2); return Mouse.UP; }
+     *
+     * if (currentGrid.canGoRight() && mapa.get(clavemapa(ratonX + 1, ratonY))
+     * == null) { memoria.push(3); return Mouse.RIGHT; } if
+     * (currentGrid.canGoLeft() && mapa.get(clavemapa(ratonX - 1, ratonY)) ==
+     * null) { memoria.push(4); return Mouse.LEFT; } if (!memoria.empty()) {
+     * return memoria.pop(); } else { mapa = new HashMap<Integer, Grid>(); }
+     * return Mouse.BOMB;
+     */
     @Override
     public void newCheese() {
     }
